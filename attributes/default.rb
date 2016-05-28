1bfbdb4c3a71859eddef1891dd1197a210680077
Jason Blalock <jasonblalock@users.noreply.github.com>
2016-05-28 16:39:35 -0400

    Add dynamic distro name to attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,7 +7,14 @@
 # MIT License
 #
 
+distro_name = case node['platform_version']
+when '14.04'
+  'trusty'
+when '16.04'
+  'xenial'
+end
+
 default['docker']['repository']['uri'] = 'https://apt.dockerproject.org/repo'
-default['docker']['platform']['distro'] = 'ubuntu-trusty'
+default['docker']['platform']['distro'] = "ubuntu-#{distro_name}"
 default['docker']['repository']['key'] = '58118E89F3A912897C070ADBF76221572C52609D'
 default['docker']['repository']['keyserver'] = 'hkp://p80.pool.sks-keyservers.net:80'
