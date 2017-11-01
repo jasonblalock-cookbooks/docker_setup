da793b2e8a0dbf54ada5db609f7fd464e74677a5
Jason Blalock <jasonblalock@users.noreply.github.com>
2017-11-01 01:17:26 -0400

    Update docker-compose version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
-default['docker_setup']['docker_compose']['release'] = '1.13.0'
+default['docker_setup']['docker_compose']['release'] = '1.16.1'
 default['docker_setup']['docker_compose']['command_path'] = '/usr/local/bin/docker-compose'
